ec8c1b4e8f45762f17bfc777ba6d2b886b31e706
Shroti Kapartiwar <skaparti@in.ibm.com>
2021-12-23 14:50:18 +0530

    added from sample

--- src/com/qa/lib/TestLib.java
+++ src/com/qa/lib/TestLib.java
@@ -7,6 +7,7 @@ public class TestLib {
 		System.out.println("changes doen as per instruction");
 		System.out.println("Changes added form Test10");
 		System.out.println("Chnages added from branch 100");
+		System.out.println("comment added from sample");
 		
 	}
 
